3b8b6cb04491b179295efeccc70f61ee2a6dc225
jaya surya <suryasarisa99.com>
2023-07-12 21:26:28 +0000

    initial

--- src/java/Java.jsx
+++ src/java/Java.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Item from "../components/Item";
 import linked_list from "./linked_list.json";
 import array from "./array";
+import stream from "./stream.json";
 import { DataContext } from "../../context/DataContext";
 export default () => {
   let { setMethods } = useContext(DataContext);
@@ -13,6 +14,7 @@ export default () => {
       <Item item="Liste Methods" lang="java" data={linked_list} />
       <Item item="String Methods" lang="java" data={strings} />
       <Item item="Array Methods" lang="java" data={array} />
+      <Item item="Stream Methods" lang="java" data={stream} />
     </div>
   );
 };
